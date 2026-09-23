752d94ca893fc85c2b986db28e1cfbe8ae0f4bbb
raouf-dv <salahr938@gmail.com>
2026-09-23 02:38:01 +0200

    Add logs for incoming requests and message status

--- routes/wheelRoutes.js
+++ routes/wheelRoutes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const wheelController = require("../controllers/wheelController");
-// 1. استيراد الحارس هنا في الأعلى (هذا هو السطر الأول الذي سألته عنه)
 const verifyFirebaseToken = require("../middlewares/authMiddleware");
 
 // حماية مسار الحالة
 router.get("/status", verifyFirebaseToken, wheelController.getWheelStatus);
 
-// حماية مسار تدوير العجلة
+// حماية مسار تدوير العجلة (يعالج اللفة العادية ولفة الإعلان معاً)
 router.post("/spin", verifyFirebaseToken, wheelController.spinWheel);
-
-// حماية مسار مشاهدة الإعلان
-router.post("/watch-ad", verifyFirebaseToken, wheelController.watchAd);
 
 module.exports = router;
